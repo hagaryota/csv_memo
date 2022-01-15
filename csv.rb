5553b0a57a9043094e2f9b1073a9736f1138e832
hagaryota <rh.pict.0723@gmail.com>
2022-01-15 13:10:19 +0000

    0115

--- csv.rb
+++ csv.rb
@@ -2,9 +2,9 @@ require "csv"
 
 puts "1(新規でメモを作成) 2(既存のメモ編集する)"
 
-memo_type = gets.to_s
+memo_type = gets.to_i
 
- if memo_type == "1"
+ if memo_type == 1
     
   puts "拡張子を除いたファイルを入力してください"
   
@@ -22,9 +22,9 @@ memo_type = gets.to_s
   end
   
   
- else memo_type == "2"
+ elsif memo_type == 2
     
-  puts "拡張子を除いたファイルを入力してください"
+  puts "拡張子を除いた既存ファイルを入力してください"
   
   file_2 = gets.chomp
   
@@ -38,5 +38,8 @@ memo_type = gets.to_s
     csv << [input]
     puts "入力内容　#{input}"
   end
+ 
+ else
+  puts "エラーです。1or2を入力してください"
  
  end
